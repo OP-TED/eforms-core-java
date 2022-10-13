126982a9ef98412d16e242f13e1b3e600d729ce1
ROUSSY Christophe (OP-EXT) <Christophe.ROUSSY@ext.publications.europa.eu>
2022-10-13 18:37:41 +0200

    TEDEFO-1415 added more constants for editor and potentially other projects. Ideally all Java projects should use these constants.

--- src/main/java/eu/europa/ted/eforms/sdk/SdkConstants.java
+++ src/main/java/eu/europa/ted/eforms/sdk/SdkConstants.java
@@ -7,6 +7,12 @@ public class SdkConstants {
   public static final String FIELDS_JSON_XML_STRUCTURE_KEY = "xmlStructure";
   public static final String FIELDS_JSON_FIELDS_KEY = "fields";
 
+  public static final String NOTICE_TYPES_JSON_DOCUMENT_TYPES_KEY = "documentTypes";
+  public static final String NOTICE_TYPES_JSON_DOCUMENT_TYPE_KEY = "documentType";
+  public static final String NOTICE_TYPES_JSON_NAMESPACE_KEY = "namespace";
+  public static final String NOTICE_TYPES_JSON_ROOT_ELEMENT_KEY = "rootElement";
+  public static final String NOTICE_CUSTOMIZATION_ID_VERSION_PREFIX = "eforms-sdk-";
+
   public static final Path DEFAULT_SDK_ROOT = Path.of("eforms-sdk");
 
   public static final String SDK_GROUP_ID = "eu.europa.ted.eforms";
@@ -17,18 +23,50 @@ public class SdkConstants {
 
   public enum SdkResource implements PathResource {
     CODELISTS(Path.of("codelists")), //
-    VIEW_TEMPLATES(Path.of("view-templates")), //
+    CODELISTS_JSON(Path.of("codelists", "codelists.json")), //
+
+    EFX_GRAMMAR(Path.of("efx-grammar")), //
+
     FIELDS(Path.of("fields")), //
-    TRANSLATIONS(Path.of("translations")), //
     FIELDS_JSON(Path.of("fields", "fields.json")), //
-    NOTICE_TYPES(Path.of("notice-types"));
+
+    NOTICE_TYPES(Path.of("notice-types")), //
+    NOTICE_TYPES_JSON(Path.of("notice-types", "notice-types.json")), //
+
+    /**
+     * XSD files.
+     */
+    SCHEMAS(Path.of("schemas")), //
+    SCHEMAS_COMMON(Path.of("schemas", "common")), //
+    SCHEMAS_MAINDOC(Path.of("schemas", "maindoc")), //
+
+    SCHEMATRONS(Path.of("schematrons")), //
+    SCHEMATRONS_DYNAMIC(Path.of("schematrons", "dynamic")), //
+    SCHEMATRONS_STATIC(Path.of("schematrons", "static")), //
+
+    /**
+     * Internal usage, tedweb.
+     */
+    TED(Path.of(".ted")), //
+    TED_TEDWEB(Path.of(".ted", "tedweb")), //
+    TED_TEDWEB_REPORT_METADATA(Path.of(".ted", "tedweb", "report-metadata.json")), //
+    TED_TEDWEB_SEARCH_METADATA(Path.of(".ted", "tedweb", "search-metadata.json")), //
+
+    /**
+     * Internationalisation, labels.
+     */
+    TRANSLATIONS(Path.of("translations")), //
+
+    VIEW_TEMPLATES(Path.of("view-templates")), //
+    VIEW_TEMPLATES_JSON(Path.of("view-templates", "view-templates.json"));
 
     private Path path;
 
-    private SdkResource(Path path) {
+    private SdkResource(final Path path) {
       this.path = path;
     }
 
+    @Override
     public Path getPath() {
       return path;
     }
