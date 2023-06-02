b53e213adbefa0e66ecf4e11b3f1d3855b075b59
Bertrand Lorentz <bertrand.lorentz@publications.europa.eu>
2023-06-02 15:27:22 +0200

    SdkVersion: Fix handling of suffix in version string (TEDEFO-2299)
    Use the Semver4j library to parse and manipulate the version.
    
    This allows to properly handle versions with a suffix like "1.2.3-rc.1"
    and "2.0.0-alpha.1"
    
    This also fixes equals() and compareTo() for versions with a suffix.
    
    Enable the unit tests that were failing before this fix.

--- src/test/java/eu/europa/ted/eforms/sdk/SdkVersionTest.java
+++ src/test/java/eu/europa/ted/eforms/sdk/SdkVersionTest.java
@@ -35,24 +35,21 @@ public class SdkVersionTest {
 		assertEquals(false, new SdkVersion("1.2").isPatch());
 		
 		assertEquals(true, new SdkVersion("1.2.3").isPatch());
-		// FIXME
-		//assertEquals(true, new SdkVersion("1.2.3-rc.4").isPatch());
+		assertEquals(true, new SdkVersion("1.2.3-rc.4").isPatch());
 	}
 
 	@Test
 	void testToNormalisedStringWithPatch() {
 		assertEquals("1.2.3", new SdkVersion("1.2.3").toNormalisedString(true));
-		// FIXME
-		//assertEquals("1.2.3", new SdkVersion("1.2.3-SNAPSHOT").toNormalisedString(true));
-		//assertEquals("1.2.3", new SdkVersion("1.2.3-rc.4").toNormalisedString(true));
+		assertEquals("1.2.3", new SdkVersion("1.2.3-SNAPSHOT").toNormalisedString(true));
+		assertEquals("1.2.3", new SdkVersion("1.2.3-rc.4").toNormalisedString(true));
 	}
 
 	@Test
 	void testToStringWithoutPatch() {
 		assertEquals("1.2", new SdkVersion("1.2.3").toStringWithoutPatch());
 		assertEquals("1.2", new SdkVersion("1.2.3-SNAPSHOT").toStringWithoutPatch());
-		// FIXME
-		//assertEquals("1.2", new SdkVersion("1.2.3-rc.4").toStringWithoutPatch());
+		assertEquals("1.2", new SdkVersion("1.2.3-rc.4").toStringWithoutPatch());
 	}
 
 	@Test
@@ -60,8 +57,7 @@ public class SdkVersionTest {
 		assert(new SdkVersion("1.2.3").compareTo(new SdkVersion("1.2.2")) > 0);
 		assert(new SdkVersion("1.2.3").compareTo(new SdkVersion("1.2")) > 0);
 		assert(new SdkVersion("1.2.3").compareTo(new SdkVersion("1.2.3-SNAPSHOT")) > 0);
-		// FIXME
-		//assert(new SdkVersion("1.2.3").compareTo(new SdkVersion("1.2.3-rc.3")) > 0);
-		//assert(new SdkVersion("2.0.0").compareTo(new SdkVersion("2.0.0-alpha.1")) > 0);
+		assert(new SdkVersion("1.2.3").compareTo(new SdkVersion("1.2.3-rc.3")) > 0);
+		assert(new SdkVersion("2.0.0").compareTo(new SdkVersion("2.0.0-alpha.1")) > 0);
 	}
 }
